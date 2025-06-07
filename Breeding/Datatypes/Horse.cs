a4ba907305b4924944394da27dc6501350601f1c
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2025-06-08 00:16:51 +0100

    Create struct and constructor

--- Breeding/Datatypes/Horse.cs
+++ Breeding/Datatypes/Horse.cs
@@ -1,6 +1,22 @@
+using System.Reflection.Metadata;
+
 namespace Breeding.Datatypes;
 
 public struct Horse
 {
-    
+    public string Name;
+    public float Pot;
+    public int Finish;
+    public float Extra;
+    public float Mat;
+
+    public Horse(string Name, float Pot, int Finish, float Extra, float Mat)
+    {
+        this.Name = Name;
+        this.Pot = Pot;
+        this.Finish = Finish;
+        this.Extra = Extra;
+        this.Mat = Mat;
+    }
+
 }
