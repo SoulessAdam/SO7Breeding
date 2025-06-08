6eb367c6e927d9a9f12df14a906f32f4f64dac4d
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2025-06-08 17:55:58 +0100

    Create filter object

---
+++ Breeding/Datatypes/Filter.cs
@@ -0,0 +1,50 @@
+using System.Text.Json.Serialization;
+
+namespace Breeding.Datatypes;
+
+public struct Filter
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("Pot")]
+    public float? minPot { private set; get; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("Finish")]
+    public int? minFin { private set; get; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("Extra")]
+    public float? minExt { private set; get; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("Mat")]
+    public float? minMat  { private set; get; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("Score")]
+    public float? minScore { private set; get; }
+
+    public float? getMin()
+    {
+        return minPot;
+    }
+
+    public int? getFin()
+    {
+        return minFin;
+    }
+
+    public float? getExtra()
+    {
+        return minExt;
+    }
+
+    public float? minMaturity()
+    {
+        return minMat;
+    }
+
+    public float? getScore()
+    {
+        return minScore;
+    }
+}
