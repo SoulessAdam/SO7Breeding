851ad8223d71f69bdc448b4415c15d472c34b52d
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2025-06-08 23:13:26 +0100

    Fix filepath issues and writing issues, parse filter and score children

--- Breeding/Program.cs
+++ Breeding/Program.cs
@@ -1,21 +1,29 @@
-﻿using Breeding.Datatypes;
+﻿using System.Text;
+using Breeding.Datatypes;
 using Breeding.Parsers;
 
 string myDocumentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 if (!Directory.Exists(Path.Join(myDocumentPath, "SO7Breeding")))
 {
     Console.WriteLine($"Initialising Directory {myDocumentPath}/SO7Breeding");
-    File.Create(Path.Join(myDocumentPath, "SO7Breeding", "Filter.json"));
-    File.Create(Path.Join(myDocumentPath, "SO7Breeding", "Mares.csv"));
-    File.Create(Path.Join(myDocumentPath, "SO7Breeding", "Stallions.csv"));
+    FileStream filterStream = File.Create(String.Join(Path.Join(myDocumentPath, "SO7Breeding"), "Filter.json"));
+    filterStream.Write(UTF8Encoding.UTF8.GetBytes("{\n  \"Pot\" : null,\n  \"Finish\" : null,\n  \"Extra\" : null,\n  \"Mat\" : null,\n  \"Score\" : null\n}"));
+    filterStream.Close();
+    
+    FileStream mareStream = File.Create(String.Join(Path.Join(myDocumentPath, "SO7Breeding"), "Mares.csv"));
+    FileStream stallionStream = File.Create(String.Join(Path.Join(myDocumentPath, "SO7Breeding"), "Stallions.csv"));
+    mareStream.Write(UTF8Encoding.UTF8.GetBytes("Name,Pot,Finish,Extra,Mat"));
+    stallionStream.Write(UTF8Encoding.UTF8.GetBytes("Name,Pot,Finish,Extra,Mat"));
+    mareStream.Close();
+    stallionStream.Close();
+    
     Console.WriteLine("Directory and files created, please fill in info accordingly before continuing...");
-    File.WriteAllText(Path.Join(myDocumentPath, "SO7Breeding", "Filter.json"), "{\n  \"Pot\" : null,\n  \"Finish\" : null,\n  \"Extra\" : null,\n  \"Mat\" : null,\n  \"Score\" : null\n}");
-    File.WriteAllText(Path.Join(myDocumentPath, "SO7Breeding", "Mares.csv"), "Name,Pot,Finish,Extra,Mat}");
-    File.WriteAllText(Path.Join(myDocumentPath, "SO7Breeding", "Stallions.csv"), "Name,Pot,Finish,Extra,Mat");
     Environment.Exit(0);
 }
 
 CsvParser cParse = new CsvParser();
+FilterParser fParse = new FilterParser();
+Filter filter = fParse.parseFilter();
 List<Horse> Stallions = new List<Horse>(cParse.ReadHorses(Path.Join(myDocumentPath, "SO7Breeding", "Stallions.csv")));
 List<Horse> Mares = new List<Horse>(cParse.ReadHorses(Path.Join(myDocumentPath, "SO7Breeding", "Mares.csv")));
 Child[] children = new Child[Stallions.Count * Mares.Count];
@@ -24,6 +32,17 @@ foreach (Horse sire in Stallions)
 {
     foreach (Horse dam in Mares)
     {
-        children[idx] = new Child(sire, dam);
+        Child foal = new Child(sire, dam);
+        if (foal.Pot < filter.getPot() || foal.Extra < filter.getExtra() || foal.Finish < filter.getFin() || foal.Score < filter.getScore())
+        {
+            continue;
+        }
+
+        children[idx] = foal;
+        idx++;
     }
+}
+foreach (Child foal in children.OrderByDescending(x => x.Score))
+{
+    Console.WriteLine(foal);
 }
