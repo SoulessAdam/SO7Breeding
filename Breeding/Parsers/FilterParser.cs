edc0b79c4654a48988086dd9cdc12ca2d4b70c88
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2025-06-09 00:18:23 +0100

    Add new JSONSerialized way of creating file

--- Breeding/Parsers/FilterParser.cs
+++ Breeding/Parsers/FilterParser.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using Breeding.Datatypes;
 
@@ -9,5 +10,12 @@ public class FilterParser
     {
         string jsonTxt = File.ReadAllText(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SO7Breeding", "Filter.json"));
         return JsonSerializer.Deserialize<Filter>(jsonTxt);
+    }
+
+    public void createExampleFilter(ref FileStream filterStream)
+    {
+        Filter newFilter = new Filter(50, 50, 75, 50, null);
+        filterStream.Write(UTF8Encoding.UTF8.GetBytes(JsonSerializer.Serialize(newFilter)));
+        filterStream.Close();
     }
 }
